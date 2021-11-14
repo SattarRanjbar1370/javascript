336a1739370e77627cdb1df3821d46c09cb92e34
sattar ranjbar <iran.kalkoush@gmail.com>
2021-11-14 21:00:45 +0330

    update loop 'do while'

--- learn/page do while/do while.js
+++ learn/page do while/do while.js
@@ -7,8 +7,16 @@ let total = 0;
 do{
     number_prompt = prompt("plese enter add: ");
     number_prompt = parseInt(number_prompt);
-    total = number_prompt + total
-    console.log("   -- "+ n + " --   ::: " + number_prompt);
-    console.log( "                     ::::>>>  " + total)
-    n++;
+    if(0 < number_prompt && number_prompt <= 20){
+        total = number_prompt + total
+        console.log("   -- "+ n + " --   ::: " + number_prompt);
+        console.log( "                     ::::>>>  " + total)
+        n++;
+    }else{
+        alert("Invalid number, please trys '0 to 20'  ");
+        number_prompt = prompt("plese enter add: ");
+        number_prompt = parseInt(number_prompt);
+    }
+    
+    
 }while(total < 50);
