6d635044a9b4baa079e234db0bfef53164d063cd
sattar ranjbar <iran.kalkoush@gmail.com>
2021-11-26 13:48:22 +0330

    function test

---
+++ learn/function/functionOne.js
@@ -0,0 +1,13 @@
+// function one
+
+
+
+function avrage(a,b,c,d){
+    if(a & b){
+        avg = (a+b)/2;
+        console.log("avrage = "+ avg);
+    }
+}
+
+main = avrage(2 , 4);
+console.log(main);
