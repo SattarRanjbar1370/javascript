9d778209a72e4e5d9f24c382d502f8d5f1837cd9
sattar ranjbar <iran.kalkoush@gmail.com>
2021-12-13 21:45:01 +0330

    end project

---
+++ if -18/app.js
@@ -0,0 +1,11 @@
+// The if and confirm commands were checked
+
+
+
+let confirmTest = confirm("Are you 18 old? ");
+
+if (confirmTest) {
+    document.getElementById('textbox').innerText = 'شما میتوانید از سایت دیدن کنید';
+} else {
+    document.getElementById('textbox').innerText = 'شما به سن قانونی نرسیدید';
+}
