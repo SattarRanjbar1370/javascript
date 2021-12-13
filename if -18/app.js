e79756d73875b6ad8170cdc3211dfb7f5743708c
sattar ranjbar <iran.kalkoush@gmail.com>
2021-12-13 22:22:33 +0330

    start project if in web

--- if -18/app.js
+++ if -18/app.js
@@ -7,5 +7,5 @@ let confirmTest = confirm("Are you 18 old? ");
 if (confirmTest) {
     document.getElementById('textbox').innerText = 'شما میتوانید از سایت دیدن کنید';
 } else {
-    document.getElementById('textbox').innerText = 'شما به سن قانونی نرسیدید';
+    document.getElementById('textbox').innerText = 'شما به سن قانونی نرسیدید    ';
 }
