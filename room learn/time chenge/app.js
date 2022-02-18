edfdbfc99896fa4485cc55e683e583aceea8e865
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-02-19 01:15:37 +0330

    Add day calculation to the app

--- room learn/time chenge/app.js
+++ room learn/time chenge/app.js
@@ -1,12 +1,20 @@
 
 
-let prompTime, timexon, timeshet, selectTime, timeLet, tagSelect;
+let prompTime, timexon, timeshet, selectTime, timeLet, tagSelect, dayCounter, dayCounter2;
 prompTime = Number(prompt('enter one noumber'));
 timexon = prompTime / 60;
 timeLet = Math.floor(timexon);
 timeshet = prompTime % 60;
-selectTime = timeLet + ' : ' + timeshet;
-console.log(selectTime);
-
+selectTime = timeLet + ' hours and ' + timeshet + ' minutes';
 tagSelect = document.getElementById('box');
-tagSelect.innerHTML = '<div class="box_center">' + selectTime + '</div>';
+
+if (timeLet > 24) {
+    dayCounter = timeLet / 24;
+    dayCounter = Math.floor(dayCounter);
+    timeLet = timeLet % 24;
+    selectTime = dayCounter + ' day and ' + timeLet + ' hours and ' + timeshet + ' minutes';
+    tagSelect.innerHTML = '<div class="box_center">' + selectTime + '</div>';
+} else {
+    tagSelect.innerHTML = '<div class="box_center">' + selectTime + '</div>';
+}
+
