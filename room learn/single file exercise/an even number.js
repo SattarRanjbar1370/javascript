f285f6e2e3f9cf27ada3fa32d53b65f100d77cf7
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-02-25 19:17:28 +0330

    Separate even numbers from odd numbers

--- room learn/single file exercise/an even number.js
+++ room learn/single file exercise/an even number.js
@@ -1,11 +1,28 @@
 // an even number
 
-let receiveNumber, validition, division;
+let receiveNumber, validition, division, oddNumber;
 
 receiveNumber = new Array();
+division = new Array();
+oddNumber = new Array();
 validition = confirm('Do you have a data to import?');
-while(validition == true){
+while (validition == true) {
     receiveNumber.push(prompt());
     validition = confirm('Do you have a data to import?');
-    console.log(receiveNumber);
+    // console.log(receiveNumber);
 }
+console.log('-.-.-.-.-.-.-.-.-.-.-.-.-.-.-');
+console.log(receiveNumber);
+console.log(receiveNumber.length);
+console.log('-.-.-.-.-.-.-.-.-.-.-.-.-.-.-');
+
+for (var i = 0; i < receiveNumber.length; i++) {
+    if (Number(receiveNumber[i] % 2) == 0) {
+        division.push(receiveNumber[i]);
+    }else{
+        oddNumber.push(receiveNumber[i]);
+    }
+}
+
+console.log('division >>> ' + oddNumber);
+console.log('oddNumber >>> ' + division);
