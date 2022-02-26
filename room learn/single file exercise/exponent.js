80de7e317cacf01c92e7d91452b6730b3f9ffe39
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-02-26 16:03:49 +0330

    calculate the numbers in the array and display them to the user

--- room learn/single file exercise/exponent.js
+++ room learn/single file exercise/exponent.js
@@ -1,15 +1,19 @@
 // exponent
 
-let basicExponent, Exponent, counter;
+let basicExponent, Exponent, counter, calculateNumbers;
 
 basicExponent = new Array();
 counter = 0;
 Exponent = 0;
-while (counter < 2){
+while (counter < 2) {
     basicExponent.push(Number(prompt('please enter one number:')));
-    if(isNaN(basicExponent[counter])){
+    if (isNaN(basicExponent[counter])) {
         basicExponent.push(Number(prompt('please try again enter one number:')));
     }
     counter++;
 }
-console.log(basicExponent)
+console.log(basicExponent);
+
+calculateNumbers = basicExponent[0] ** basicExponent[1];
+
+console.log(basicExponent[0] + ' ** ' + basicExponent[1] + ' = ' + calculateNumbers);
