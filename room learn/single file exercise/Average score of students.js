b720c7666ad5a97a0d5a80f4b7c08ee973f20440
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-02-26 20:47:13 +0330

    write loop counter and receive data in loop

---
+++ room learn/single file exercise/Average score of students.js
@@ -0,0 +1,16 @@
+// average score of studdent ...!!
+
+let score, average, receivedData, counter;   //  Computable variables
+// receivedData = 0;
+counter = 0;
+score = new Array();
+while (score[counter - 1] != -1) {
+    score.push(Number(prompt('please enter one number:\n Enter -1 to exit!')));   //  Push data received from the user
+    while (isNaN(score[counter])) {
+        score.push(Number(prompt('please try again enter one number:\n Enter -1 to exit!')));
+    }
+    counter++;
+    console.log(counter + ' <<<<<<');
+}
+score.pop();
+console.log(score)
