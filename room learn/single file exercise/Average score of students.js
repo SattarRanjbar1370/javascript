8456b25de340f8ea260d4df49715f2580a254975
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-02-26 21:08:33 +0330

    calculate the average of user data

--- room learn/single file exercise/Average score of students.js
+++ room learn/single file exercise/Average score of students.js
@@ -1,17 +1,21 @@
 // average score of studdent ...!!
 
-let score, average, receivedData, counter;   //  Computable variables
+let score, average, receivedData, counter, sumOfNumber;   //  Computable variables
 // receivedData = 0;
 counter = 0;
 score = new Array();
 while (score[counter - 1] != -1) {
     score.push(Number(prompt('please enter one number:\n Enter -1 to exit!')));   //  Push data received from the user
     while (isNaN(score[counter])) {
-        // score.push(Number(prompt('please try again enter one number:\n Enter -1 to exit!')));
         score[counter] = Number(prompt('please try again enter one number:\n Enter -1 to exit!'));
     }
     counter++;
     console.log(counter + ' <<<<<<');
 }
-score.pop();
-console.log(score)
+score.pop(); // remove the loop stop condition
+sumOfNumber = 0;
+for (counter = 0; counter < score.length; counter++) {
+    sumOfNumber += score[counter];
+}
+console.log('average: ', sumOfNumber / counter);
+console.log(score);
