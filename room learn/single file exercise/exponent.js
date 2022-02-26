f9b7a60a27ad7332f1485705294c84b3137c055d
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-02-26 15:47:12 +0330

    Loop for receiving data from the user

--- room learn/single file exercise/exponent.js
+++ room learn/single file exercise/exponent.js
@@ -1,3 +1,15 @@
 // exponent
 
 let basicExponent, Exponent, counter;
+
+basicExponent = new Array();
+counter = 0;
+Exponent = 0;
+while (counter < 2){
+    basicExponent.push(Number(prompt('please enter one number:')));
+    if(isNaN(basicExponent[counter])){
+        basicExponent.push(Number(prompt('please try again enter one number:')));
+    }
+    counter++;
+}
+console.log(basicExponent)
