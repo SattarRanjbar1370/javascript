7b592a5c397792d22b939d483bfefbaa0c508b07
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-02-26 20:54:31 +0330

    debuging

--- room learn/single file exercise/Average score of students.js
+++ room learn/single file exercise/Average score of students.js
@@ -7,7 +7,8 @@ score = new Array();
 while (score[counter - 1] != -1) {
     score.push(Number(prompt('please enter one number:\n Enter -1 to exit!')));   //  Push data received from the user
     while (isNaN(score[counter])) {
-        score.push(Number(prompt('please try again enter one number:\n Enter -1 to exit!')));
+        // score.push(Number(prompt('please try again enter one number:\n Enter -1 to exit!')));
+        score[counter] = Number(prompt('please try again enter one number:\n Enter -1 to exit!'));
     }
     counter++;
     console.log(counter + ' <<<<<<');
