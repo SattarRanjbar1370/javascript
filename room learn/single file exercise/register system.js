7a1f56cbad91ef766d886793fd3bfb4c6b4c6938
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-03 23:40:10 +0330

    remove incorrect cods

--- room learn/single file exercise/register system.js
+++ room learn/single file exercise/register system.js
@@ -7,26 +7,14 @@ dataBase = new Array(
     { name: 'hassan', familly: 'Ranjbar', age: 25, email: 'hassan74ranjbar@gmail.com' }
 );
 
-counter = 0;
-stopCondition = confirm('Do you have another member to register?');
-while (stopCondition == true) {
-    dataBase.unshift({});
-    dataBase[0].name = prompt('name:');
-    // while (10 < dataBase[0].name.length < 3){
-    //     dataBase[0].name = prompt('try entring the name again:');
-    // }
-    dataBase[0].familly = prompt('familly:');
-    dataBase[0].age = Number(prompt('age:'));
-    dataBase[0].email = prompt('email:');
-    stopCondition = confirm('Do you have another member to register?');
-}
+// counter = 0;
+// stopCondition = confirm('Do you have another member to register?');
+// while (stopCondition == true) {
+    
+// }
 
 console.log(dataBase);
 
 ///////////////////////////////////////////////////////////////////////
 ////////////////////////////// display ////////////////////////////////
 ///////////////////////////////////////////////////////////////////////
-
-let bodyTag, divContiner, divBox;
-bodyTag = document.getElementById('body').innerHTML = '<div id="continer" class="continer"></div>';
-divContiner = document.getElementById('continer').innerHTML = '<div class="box">Hi, good time<br>please log in to the console to see the result.</div>';
