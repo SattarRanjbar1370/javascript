7ff678cf2b4ccd3925536b6655d74781fe5b7c0b
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-04 00:09:24 +0330

    add loops to test data

--- room learn/single file exercise/register system.js
+++ room learn/single file exercise/register system.js
@@ -8,8 +8,17 @@ dataBase = new Array(
 );
 
 let userName = prompt('please enter your name: ');
+while (userName.length > 10 || userName.length < 3) {
+    userName = prompt('The name should be from 3 to 10 characters, try again:');
+}
 let userFamily = prompt('please enter your family: ');
+while (userFamily.length > 15 || userFamily.length < 4) {
+    userFamily = prompt('The family should be from 4 to 15 characters, try again:');
+}
 let userAge = prompt('please enter your age: ');
+while (isNaN(userAge)) {
+    userAge = prompt('Enter the age correctly, age must be a number. try again: ');
+}
 let userEmail = prompt('please enter your email: ');
 
 // counter = 0;
