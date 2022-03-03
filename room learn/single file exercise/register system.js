aa10cbcf388dce3424f57b6de40dfff60403058c
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-04 00:19:03 +0330

    add object

--- room learn/single file exercise/register system.js
+++ room learn/single file exercise/register system.js
@@ -1,27 +1,35 @@
 // Register system
 
-let dataBase, counter, stopCondition;
+let dataBase, counter, stopCondition, userName, userFamily, userAge, userEmail, objectModel;
 dataBase = new Array(
-    { name: 'sattar', familly: 'Ranjbar', age: 30, email: 'iran.kalkoush@gmail.com' },
-    { name: 'nima', familly: 'lotfi', age: 23, email: 'nima.lotfi@gmail.com' },
-    { name: 'hassan', familly: 'Ranjbar', age: 25, email: 'hassan74ranjbar@gmail.com' }
+    { id: 1, name: 'sattar', family: 'Ranjbar', age: 30, email: 'iran.kalkoush@gmail.com' },
+    { id: 2, name: 'nima', family: 'lotfi', age: 23, email: 'nima.lotfi@gmail.com' },
+    { id: 3, name: 'hassan', family: 'Ranjbar', age: 25, email: 'hassan74ranjbar@gmail.com' }
 );
 
-let userName = prompt('please enter your name: ');
+userName = prompt('please enter your name: ');
 while (userName.length > 10 || userName.length < 3) {
     userName = prompt('The name should be from 3 to 10 characters, try again:');
 }
-let userFamily = prompt('please enter your family: ');
+userFamily = prompt('please enter your family: ');
 while (userFamily.length > 15 || userFamily.length < 4) {
     userFamily = prompt('The family should be from 4 to 15 characters, try again:');
 }
-let userAge = prompt('please enter your age: ');
+userAge = prompt('please enter your age: ');
 while (isNaN(userAge)) {
     userAge = prompt('Enter the age correctly, age must be a number. try again: ');
 }
-let userEmail = prompt('please enter your email: ');
+userEmail = prompt('please enter your email: ');
 
-// counter = 0;
+objectModel = {
+    id = counter+1,
+    name: userName,
+    family: userFamily,
+    age: Number(userAge),
+    email: userEmail
+};
+
+// counter = 3;
 // stopCondition = confirm('Do you have another member to register?');
 // while (stopCondition == true) {
 
