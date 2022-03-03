3097391ff885942ee63b4a7c4a9f018bedb0153d
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-03 23:49:54 +0330

    received data from the user

--- room learn/single file exercise/register system.js
+++ room learn/single file exercise/register system.js
@@ -7,10 +7,15 @@ dataBase = new Array(
     { name: 'hassan', familly: 'Ranjbar', age: 25, email: 'hassan74ranjbar@gmail.com' }
 );
 
+let userName = prompt('please enter your name: ');
+let userFamily = prompt('please enter your family: ');
+let userAge = prompt('please enter your age: ');
+let userEmail = prompt('please enter your email: ');
+
 // counter = 0;
 // stopCondition = confirm('Do you have another member to register?');
 // while (stopCondition == true) {
-    
+
 // }
 
 console.log(dataBase);
