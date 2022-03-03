d23be8cf8481d22b12a01a3649c976a3ab862406
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-04 00:21:00 +0330

    Activate the database loop

--- room learn/single file exercise/register system.js
+++ room learn/single file exercise/register system.js
@@ -7,33 +7,34 @@ dataBase = new Array(
     { id: 3, name: 'hassan', family: 'Ranjbar', age: 25, email: 'hassan74ranjbar@gmail.com' }
 );
 
-userName = prompt('please enter your name: ');
-while (userName.length > 10 || userName.length < 3) {
-    userName = prompt('The name should be from 3 to 10 characters, try again:');
-}
-userFamily = prompt('please enter your family: ');
-while (userFamily.length > 15 || userFamily.length < 4) {
-    userFamily = prompt('The family should be from 4 to 15 characters, try again:');
-}
-userAge = prompt('please enter your age: ');
-while (isNaN(userAge)) {
-    userAge = prompt('Enter the age correctly, age must be a number. try again: ');
-}
-userEmail = prompt('please enter your email: ');
 
-objectModel = {
-    id = counter+1,
-    name: userName,
-    family: userFamily,
-    age: Number(userAge),
-    email: userEmail
-};
 
-// counter = 3;
-// stopCondition = confirm('Do you have another member to register?');
-// while (stopCondition == true) {
+counter = 3;
+stopCondition = confirm('Do you have another member to register?');
+while (stopCondition == true) {
+    userName = prompt('please enter your name: ');
+    while (userName.length > 10 || userName.length < 3) {
+        userName = prompt('The name should be from 3 to 10 characters, try again:');
+    }
+    userFamily = prompt('please enter your family: ');
+    while (userFamily.length > 15 || userFamily.length < 4) {
+        userFamily = prompt('The family should be from 4 to 15 characters, try again:');
+    }
+    userAge = prompt('please enter your age: ');
+    while (isNaN(userAge)) {
+        userAge = prompt('Enter the age correctly, age must be a number. try again: ');
+    }
+    userEmail = prompt('please enter your email: ');
 
-// }
+    objectModel = {
+        id = counter + 1,
+        name: userName,
+        family: userFamily,
+        age: Number(userAge),
+        email: userEmail
+    };
+    dataBase.push(objectModel);
+}
 
 console.log(dataBase);
 
