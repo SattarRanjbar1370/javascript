d4deae59b3782f7bff66f81dace1d76c9c23e349
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-04 16:23:26 +0330

    update object loop >>> add price

--- room learn/single file exercise/online store on the console.js
+++ room learn/single file exercise/online store on the console.js
@@ -1,21 +1,25 @@
 // online store on the console !!!!!
 
-let listShoping, products, productCounter, shopingCounter, productCondition, productName, id, productObject;
+let listShoping, products, productCounter, shopingCounter, productCondition, productName, productPrice, id, productObject;
 
 products = new Array(
-    { id: 1, name: 'pen' },
-    { id: 2, name: 'book' },
-    { id: 3, name: 'body splash' },
-    { id: 4, name: 'note book' }
+    { id: 1, name: 'pen', price: 8000 },
+    { id: 2, name: 'book', price: 12000 },
+    { id: 3, name: 'body splash', price: 9000 },
+    { id: 4, name: 'note book', price: 14000 }
 );
 productCondition = confirm('Do you have products to introduce?');
-productCounter = 3;
+productCounter = 4;
 while (productCondition == true) {
     productName = prompt('enter the product name:');
     id = productCounter + 1;
+    productPrice = Number(prompt('enter the product price:'));
     productObject = {
         id: id,
-        name: productName
+        name: productName,
+        price: productPrice
     }
     products.push(productObject);
+    productCondition = confirm('Do you have products to introduce?');
+    productCounter++;
 }
