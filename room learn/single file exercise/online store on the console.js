22c26c5d97cc035d6686dae3bfa316c81006e129
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-04 15:53:20 +0330

    product introdaction and download

---
+++ room learn/single file exercise/online store on the console.js
@@ -0,0 +1,14 @@
+// online store on the console !!!!!
+
+let listShoping, products, productCounter, shopingCounter, productCondition;
+
+products = new Array(
+    { id: 1, name: 'pen' },
+    { id: 2, name: 'book' },
+    { id: 3, name: 'body splash' },
+    { id: 4, name: 'note book' }
+);
+productCondition = confirm('Do you have products to introduce?');
+ while(productCondition == true){
+    
+ }
