93a9aacbbd39c416e0da0fb935c67dc1c40cd281
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-05 10:57:45 +0330

    show products to the user

--- room learn/single file exercise/online store on the console.js
+++ room learn/single file exercise/online store on the console.js
@@ -1,6 +1,12 @@
 // online store on the console !!!!!
 
+/////////////// variable ////////////////
+
 let listShoping, products, productCounter, shopingCounter, productCondition, productName, productPrice, id, productObject;
+
+////////////////////////////////////////
+//////////// product list //////////////
+////////////////////////////////////////
 
 products = new Array(
     { id: 1, name: 'pen', price: 8000 },
@@ -8,6 +14,9 @@ products = new Array(
     { id: 3, name: 'body splash', price: 9000 },
     { id: 4, name: 'note book', price: 14000 }
 );
+
+//////////// product loop //////////////
+
 productCondition = confirm('Do you have products to introduce?');
 productCounter = 4;
 while (productCondition == true) {
@@ -26,3 +35,24 @@ while (productCondition == true) {
     productCondition = confirm('Do you have products to introduce?');
     productCounter++;
 }
+
+///////////////////////////////////////////
+//////////// product display //////////////
+///////////////////////////////////////////
+
+/////////////// variable //////////////////
+
+let bodyTag, divContiner, divBox, displayUser, varDisplay;
+
+products.forEach(function (item) {
+    displayUser = 'id: ' + item.id + '\t\tproduct: ' + item.name + '\t\t\tprice: ' + item.price;
+    console.log(displayUser);
+    varDisplay = '<div class="box" id="box">' + displayUser + '</div>';
+});
+bodyTag = document.getElementById('body').innerHTML = '<div class="continer" id="continer"></div>';
+divContiner = document.getElementById('continer').innerHTML = varDisplay;
+
+///////////////////////////////////////////
+////////////// user shoping ///////////////
+///////////////////////////////////////////
+
