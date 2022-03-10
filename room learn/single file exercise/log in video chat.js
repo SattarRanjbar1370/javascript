1634ab02df1a2daa9ca49aa29f26df5c7133f633
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-10 23:16:45 +0330

    add members to the video chat app

---
+++ room learn/single file exercise/log in video chat.js
@@ -0,0 +1,19 @@
+// condition  for enter the  video chat program
+
+let dataBase = [
+    { id: 1, userName: 'sattar70', name: 'sattar', age: 30 },
+    { id: 2, userName: 'alkds', name: 'ali kocholo', age: 19 },
+    { id: 3, userName: 'smal340', name: 'sama', age: 42 },
+    { id: 4, userName: 'beost', name: 'yobs one', age: 30 },
+    { id: 5, userName: 'ddlskd', name: 'dige neist', age: 17 },
+    { id: 6, userName: 'load_cony', name: 'kordi', age: 25 },
+    { id: 7, userName: 'hesam', name: 'hesam', age: 24 },
+    { id: 8, userName: 'staridi', name: 'setari', age: 24 },
+    { id: 9, userName: 'sdlllil', name: 'salar', age: 31 },
+    { id: 10, userName: '__fanos__', name: 'mansa', age: 37 },
+    { id: 11, userName: 'komile', name: 'sara', age: 34 },
+    { id: 12, userName: 'ojagh', name: 'hosna', age: 33 },
+    { id: 13, userName: 'big_like', name: 'ali', age: 30 },
+    { id: 14, userName: 'torbat_jam', name: 'torbat', age: 14 },
+    { id: 15, userName: 'mmali', name: 'mmadi', age: 19 }
+];
