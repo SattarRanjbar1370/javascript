da191104cac22429edfb2c119049069ff98d4f47
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-21 02:41:13 +0330

    add array to 'shop room.js' file

---
+++ room learn/single file exercise/shop room.js
@@ -0,0 +1,14 @@
+// shop room
+
+let shopRoom = [
+    { id: 1, name: 'pen', price: 8000 },
+    { id: 2, name: 'book', price: 12000 },
+    { id: 3, name: 'body splash', price: 9000 },
+    { id: 4, name: 'note book', price: 14000 },
+    { id: 1, name: 'Pencil', price: 8000 },
+    { id: 2, name: 'tractor', price: 12000 },
+    { id: 3, name: 'Mac Book', price: 9000 },
+    { id: 4, name: 'watch', price: 14000 }
+];
+
+console.log(shopRoom);
