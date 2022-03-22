e4fc6c0b5a73b5bb668b2927f9631f03bb0b82a1
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-22 04:58:14 +0430

    store modification, chenge condition one in loop

--- room learn/single file exercise/shop room.js
+++ room learn/single file exercise/shop room.js
@@ -5,24 +5,18 @@ let shopRoom = [ // array back end for shoping
     { id: 2, name: 'book', price: 12000 },
     { id: 3, name: 'body splash', price: 9000 },
     { id: 4, name: 'note book', price: 14000 },
-    { id: 1, name: 'Pencil', price: 8000 },
-    { id: 2, name: 'tractor', price: 12000 },
-    { id: 3, name: 'Mac Book', price: 9000 },
-    { id: 4, name: 'watch', price: 14000 }
+    { id: 5, name: 'Pencil', price: 7000 },
+    { id: 6, name: 'tractor', price: 12000 },
+    { id: 7, name: 'Mac Book', price: 1500000 },
+    { id: 8, name: 'watch', price: 14000 }
 ];
-
 console.log(shopRoom);
-
 let cartUser = [    // array frant for user cart
     { id: 2, name: 'book', price: 12000 },
     { id: 2, name: 'tractor', price: 12000 }
 ];
-
 console.log(cartUser);
-
-let receiveBuy, nameItem, selectItem, addToCartUser;
-
-
+let receiveBuy, nameItem, selectItem, addToCartUser, cartUserTest;
 let loadLoop = confirm("Click 'ok' if you want to edit your shoping cart");
 
 
@@ -30,23 +24,40 @@ while (loadLoop == true) {
     selectItem = prompt('Enter \'1\' if you want to add and  \'2\' if you want to delete');
     if (selectItem == 1) {
         nameItem = prompt('please enter name item for add');
-        receiveBuy = shopRoom.findIndex(function (user) {
-            return user.name === nameItem
+        cartUserTest = cartUser.some(function(user){
+            return user.name == nameItem
         });
-        addToCartUser = shopRoom[receiveBuy];
-        cartUser.unshift(addToCartUser);
+        if (cartUserTest == true) {
+            // receiveBuy = shopRoom.findIndex(function(user){
+            //     return user.name == nameItem
+            //   });
+            shopRoom.forEach(function(user){
+                addToCartUser = user;
+                return user.name == nameItem
+            })
+            // addToCartUser = shopRoom[receiveBuy];
+            cartUser.push(addToCartUser);
+        } else {
+            console.log('error ...!!\n no item :(')
+        }
+
     } else if (selectItem == 2) {
+
         nameItem = prompt('please enter name item for delete');
         receiveBuy = cartUser.findIndex(function (user) {
             return user.name === nameItem
         });
-        cartUser.splice(receiveBuy , 1);
+        cartUser.splice(receiveBuy, 1);
+
     }
-    console.log(cartUser);
+
+
+    // console.log(cartUser);
+
     // selectItem = prompt('Ente \'2\' if you want to delete');
 
     loadLoop = confirm("Click 'ok' if you want to edit your shoping cart");
 }
-
+console.log('       ****        ')
 console.log(cartUser);
 
