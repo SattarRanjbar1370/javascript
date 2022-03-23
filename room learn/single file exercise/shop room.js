c8dba291edd6cd3338dd3dd8f7d965d4fea05753
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-23 18:48:03 +0430

    Total sum and calculation of postage

--- room learn/single file exercise/shop room.js
+++ room learn/single file exercise/shop room.js
@@ -18,8 +18,6 @@ let cartUser = [    // array frant for user cart
 console.log(cartUser);
 let receiveBuy, nameItem, selectItem, addToCartUser, cartUserTest;
 let loadLoop = confirm("Click 'ok' if you want to edit your shoping cart");
-
-
 while (loadLoop == true) {
     selectItem = prompt('Enter \'1\' if you want to add and  \'2\' if you want to delete');
     if (selectItem == 1) {
@@ -37,7 +35,6 @@ while (loadLoop == true) {
         } else {
             console.log('error ...!!\n no item :(')
         }
-
     } else if (selectItem == 2) {
         nameItem = prompt('please enter name item for delete');
         cartUserTest = cartUser.some(function (item) {
@@ -52,14 +49,20 @@ while (loadLoop == true) {
     } else {
         console.log('please enter a valid option')
     }
-
-
-    // console.log(cartUser);
-
-    // selectItem = prompt('Ente \'2\' if you want to delete');
-
     loadLoop = confirm("Click 'ok' if you want to edit your shoping cart");
 }
 console.log('       ****        ')
 console.log(cartUser);
 
+let cartUserSmall = cartUser.filter(function (item) {
+    return item.price < 10000
+});
+
+let sumLength = cartUserSmall.length * 1000;
+
+let sumCartUser = 0;
+cartUser.forEach(function (item) {
+    sumCartUser += item.price;
+});
+
+console.log('sum price:  ', sumLength + sumCartUser);
