3aab3847f2706bc7da40b4d99bf45bfad5c14191
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-24 15:13:23 +0430

    add list management

---
+++ room learn/single file exercise/13 task management.js
@@ -0,0 +1,9 @@
+
+let listManagement, management;
+
+listManagement = [
+    { id: 1, nameUser: 'mohammad', task: 'ropeacces' },
+    { id: 2, nameUser: 'ahmadi', task: 'elevator tecnesian' },
+    { id: 3, nameUser: 'sadeghi', task: 'gardener' },
+    { id: 4, nameUser: 'ghasemi', task: 'security' },
+];
