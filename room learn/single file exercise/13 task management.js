e5a2a1b0c5746f608e2bb96895131ec6253ee672
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-24 15:24:56 +0430

    add user name request loop

--- room learn/single file exercise/13 task management.js
+++ room learn/single file exercise/13 task management.js
@@ -1,5 +1,5 @@
 
-let listManagement, management;
+let listManagement, management, task, userTask, objecTask;
 
 listManagement = [
     { id: 1, nameUser: 'mohammad', task: 'ropeacces' },
@@ -7,3 +7,18 @@ listManagement = [
     { id: 3, nameUser: 'sadeghi', task: 'gardener' },
     { id: 4, nameUser: 'ghasemi', task: 'security' },
 ];
+console.log('list management: ',listManagement);
+management = confirm('Do you add a user?');
+
+while (management) {
+    task = prompt('write a task: ');
+    userTask = prompt('write the user name of the task:');
+    objecTask = {
+        id: listManagement.length + 1,
+        nameUser: userTask,
+        task: task
+    }
+    listManagement.push(objecTask);
+    management = confirm('Do you add a user again?');
+}
+console.log('list management: ',listManagement);
