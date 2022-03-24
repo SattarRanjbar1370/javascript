3fecf908260e7e26ab15fac0380331ac914ba827
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-24 13:14:24 +0430

    condition for deleting a plan

--- room learn/single file exercise/12 to do list.js
+++ room learn/single file exercise/12 to do list.js
@@ -7,7 +7,8 @@ let toDoList = [
 
 console.log('to do list: ', toDoList);
 let optionList = prompt('1. add reminder\n2. remove reminder\n3. chenge status');
-let plan, sendToDoList;
+let plan, sendToDoList, testToDoList, arrayIndx;
+
 if (optionList == 1) {
     plan = prompt('write a plan: ');
     sendToDoList = {
@@ -17,4 +18,20 @@ if (optionList == 1) {
     }
     toDoList.push(sendToDoList);
     console.log('to do list: ', toDoList);
-}
+} else if (optionList == 2) {
+    plan = prompt('enter name plane: ');
+    testToDoList = toDoList.some(function (item) {
+        return item.text == plan
+    });
+    if (testToDoList) {
+        arrayIndx = toDoList.findIndex(function (item) {
+            return item.text == plan
+        });
+        toDoList.splice(arrayIndx, 1);
+        console.log('to do list: ', toDoList);
+    } else {
+        console.log('input error\nplease try again');
+    }
+} 
+
+// console.log(arrayIndx);
