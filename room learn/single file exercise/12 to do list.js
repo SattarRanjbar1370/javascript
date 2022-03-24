3305d22f3de8ae9b4c43c2997dc3e5c86905d9c8
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-24 08:01:36 +0430

    Add array to list

---
+++ room learn/single file exercise/12 to do list.js
@@ -0,0 +1,8 @@
+
+let toDoList = [
+    { id: 1, text: 'sport', Status: true },
+    { id: 2, text: 'contant the father', Status: true },
+    { id: 3, text: 'art class', Status: false }
+];
+
+let optionList = prompt('1. add reminder\n2. remove reminder\n3. chenge status');
