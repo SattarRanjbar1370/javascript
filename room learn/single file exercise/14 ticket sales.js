c4e5ee8da92a52da743c68a16f3097cecbf9e173
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-24 18:30:44 +0430

    add data base in project

---
+++ room learn/single file exercise/14 ticket sales.js
@@ -0,0 +1,11 @@
+
+let ticketSales = {
+    tehran: ['azadi', 'terminal jnob', 'karaj', 'pardis'],
+    kermanshah: ['kermanshah', 'eslam abad', 'gilangharb'],
+    hamedan: ['hamedan', 'asad abad'],
+    oromieh: ['oromieh', 'mako', 'sardasht', 'piranshahr'],
+    kordestan: ['sanndej', 'bane'],
+    khozestan: ['ahvaz', 'ramhormoz', 'khoram shahr']
+};
+
+console.log('ticket sales: ', ticketSales);
