a6f96bed442d3d3841c1a90ec7361daa9db6483b
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-24 12:37:57 +0430

    condition, add work to list

--- room learn/single file exercise/12 to do list.js
+++ room learn/single file exercise/12 to do list.js
@@ -5,4 +5,16 @@ let toDoList = [
     { id: 3, text: 'art class', Status: false }
 ];
 
+console.log('to do list: ', toDoList);
 let optionList = prompt('1. add reminder\n2. remove reminder\n3. chenge status');
+let plan, sendToDoList;
+if (optionList == 1) {
+    plan = prompt('write a plan: ');
+    sendToDoList = {
+        id: toDoList.length + 1,
+        text: plan,
+        Status: false
+    }
+    toDoList.push(sendToDoList);
+    console.log('to do list: ', toDoList);
+}
