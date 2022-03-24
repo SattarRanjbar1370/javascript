0c62013e3182b88f0a709ef56498570d67c37fbe
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-25 00:51:52 +0430

    completed '14 ticket sales.js' project

--- room learn/single file exercise/14 ticket sales.js
+++ room learn/single file exercise/14 ticket sales.js
@@ -1,4 +1,5 @@
 
+let arrayCityList, arrayCityToList, counter, choiceCity;
 let ticketSales = {
     tehran: ['azadi', 'terminal jnob', 'karaj', 'pardis'],
     kermanshah: ['kermanshah', 'eslam abad', 'gilangharb'],
@@ -9,3 +10,7 @@ let ticketSales = {
 };
 
 console.log('ticket sales: ', ticketSales);
+let listCity = prompt('enter a valid name:\n 1. tehran\n 2. kermanshah\n 3. hamedan\n 4. oromieh\n 5. kordestan\n 6. khozestan');
+
+choiceCity = ticketSales[listCity];
+console.log('list city: \n', choiceCity.join('\n'));
