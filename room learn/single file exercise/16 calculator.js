751e9cf814af66a000bb7a1ce4dd19b16a0e1713
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-25 12:58:33 +0430

    create 'switch case' for project

--- room learn/single file exercise/16 calculator.js
+++ room learn/single file exercise/16 calculator.js
@@ -9,4 +9,23 @@ while (isNaN(numberTwo)) {
     numberTwo = Number(prompt('please try again, \ninput number was invalid'));
 }
 operator = prompt('select an operator to calculate:\n 1. *\n 2. / \n 3. + \n 4. -');
-
+switch (operator) {
+    case '1':
+        showUser = numberOne * numberTwo;
+        alert('The result: ' + showUser);
+        break;
+    case '2':
+        showUser = numberOne / numberTwo;
+        alert('The result: ' + showUser);
+        break;
+    case '3':
+        showUser = numberOne + numberTwo;
+        alert('The result: ' + showUser);
+        break;
+    case '4':
+        showUser = numberOne - numberTwo;
+        alert('The result: ' + showUser);
+        break;
+    default:
+        alert('Undefined value');
+}
