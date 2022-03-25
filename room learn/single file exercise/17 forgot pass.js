546de5542c148aa69ad85c429e41501c126a8fd2
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-25 15:49:30 +0430

    create database

---
+++ room learn/single file exercise/17 forgot pass.js
@@ -0,0 +1,12 @@
+
+let dataBase = [
+    {id: 1,userName: 'mohammad', pass: 'jd45322f'},
+    {id: 2,userName: 'sattar', pass: 'hgmk5643'},
+    {id: 3,userName: 'reza', pass: '0125reza'},
+    {id: 4,userName: 'amin', pass: 'kamkam32'},
+    {id: 5,userName: 'hassan', pass: 'a1234567'},
+    {id: 6,userName: 'ali', pass: 'loadpass'},
+    {id: 7,userName: 'mostafa', pass: 'thHkF123'},
+];
+
+
