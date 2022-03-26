69cb6addc530b8076e261a78735155f9aefa7f1f
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-26 18:47:52 +0430

    difine countdown variables

--- room learn/single file exercise/18 timer/app.js
+++ room learn/single file exercise/18 timer/app.js
@@ -1,7 +1,12 @@
 
-let timerReverse, elementID, i;
+let timerReverse, elementID, counterSecond, counterMin, counterHour;
+counterHour = +prompt('enter the time (hour) from 1 to 24:');
+counterMin = +prompt('enter the time (minute) from 1 to 60: ');
+counterSecond = +prompt('enter the time (second) from 1 to 60: ');
+
+
 elementID = document.getElementById('Seconds')
-i = 30
+i = 59
 timerReverse = setInterval(function () {
     if (i === 0) {
         clearInterval(timerReverse)
