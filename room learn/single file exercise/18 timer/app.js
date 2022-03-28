bf2f95d71ddc978eca2b3193fe2cde4dfabd3797
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-28 15:58:13 +0430

    complete project reverse timer

--- room learn/single file exercise/18 timer/app.js
+++ room learn/single file exercise/18 timer/app.js
@@ -1,17 +1,33 @@
 
-let timerReverse, elementID, counterSecond, counterMin, counterHour;
-counterHour = +prompt('enter the time (hour) from 1 to 24:');
+let timerReverse, elementID, elementIdMin, counterSecond, counterMin, counterHour, sec;
+elementID = document.getElementById('Seconds');
+elementIdMin = document.getElementById('minuts');
+
+
 counterMin = +prompt('enter the time (minute) from 1 to 60: ');
+while (counterMin <= 0 && counterMin >= 59) {
+    counterHour = prompt('error \nenter the time (minute) from 1 to 59:');
+}
 counterSecond = +prompt('enter the time (second) from 1 to 60: ');
+while (counterSecond <= 0 && counterSecond >= 59) {
+    counterHour = prompt('error \nenter the time (second) from 1 to 59:');
+}
 
-
-elementID = document.getElementById('Seconds')
-i = 59
 timerReverse = setInterval(function () {
-    if (i === 0) {
-        clearInterval(timerReverse)
+    if (counterSecond !== 0) {
+        sec = counterSecond;
+        counterSecond = 0;
+    } else if (sec === -1) {
+        counterMin--;
+        sec = 59;
     }
-    console.log(i)
-    elementID.innerText = i;
-    i--
+
+    if (sec === 0 && counterMin === 0) {
+        clearInterval(timerReverse);
+    }
+
+
+    elementID.innerText = sec;
+    elementIdMin.innerText = counterMin;
+    sec--;
 }, 1000);
