08fa51606660c2d96b798c30d607d7c6ddf4b5e8
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-29 01:41:21 +0430

    update login one name project

--- room learn/login only one name/root/app.js
+++ room learn/login only one name/root/app.js
@@ -8,7 +8,7 @@ box_error_pass = document.getElementById('box_log');
 box_log = document.getElementById('box_log');
 
 userName = 'ali';
-password = 'Sattar0132831';
+password = 'sattar0132831';
 userNameLoad = prompt('please enter your name:');
 
 if (userNameLoad.toLowerCase() == userName) {
@@ -24,3 +24,31 @@ if (userNameLoad.toLowerCase() == userName) {
 } else {
     box_error_user.innerHTML = '<div class="error-user error" id="error-user">error user name, please try</div>';
 }
+
+let arrayItem, div2, listNameOne, showArray, cunter, bodyItem;
+arrayItem = ['sattar', 'hassan', 'ali'];
+div2 = document.createElement('div');
+// div2.setAttribute('id', 'dox');
+div2.setAttribute('class', 'box box-filter');
+listNameOne = document.createElement('div');
+cunter = 0;
+bodyItem = document.getElementsByTagName('body');
+console.log(bodyItem);
+// bodyItem[0].append(div2);
+div_base.append(div2);
+
+console.log(div2);
+
+let i = arrayItem.length;
+
+showArray = setInterval(function () {
+    if (cunter === i - 1) {
+        clearInterval(showArray);
+    }
+    div2.append(arrayItem[cunter] + ', ');
+    cunter++;
+}, 3000)
+// setTimeout(() => {
+
+
+// }, 6000);
