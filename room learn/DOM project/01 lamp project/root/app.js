12120b024262ea7a3a318c3743827c2ec2b5dc0c
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-03-29 18:40:52 +0430

    create on and off lamp function

---
+++ room learn/DOM project/01 lamp project/root/app.js
@@ -0,0 +1,11 @@
+
+let imgSelect = document.getElementById('image');
+
+function cleckTurnOn() {
+    imgSelect.src = 'image/turn on.png';
+}
+function clickTurnOff(){
+    imgSelect.src = 'image/turn of.png';
+}
+
+
