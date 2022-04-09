a80c5b3e830fb7784711085533c1d27e943ed0b5
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-04-09 23:55:20 +0430

    complet project

---
+++ room learn/DOM project/03 form login/root/app.js
@@ -0,0 +1,31 @@
+//
+
+let selectTag = document.getElementById('alert');
+let selectInput = document.querySelectorAll('input');
+// console.log(selectInput[0],selectInput[1])
+
+function setAlertText() {
+    if (selectInput[0].value.length < 4) {
+        selectTag.style.display = 'block';
+        selectTag.style.background = 'red';
+        selectTag.innerText = 'error character';
+    } else if (selectInput[0].value.length >= 4) {
+        // selectTag.style.display = 'block';
+        selectTag.style.background = 'rgb(35, 149, 230)';
+        selectTag.innerText = 'true text';
+    }
+}
+
+function setAlertPass() {
+    if (selectInput[1].value.length < 4) {
+        selectTag.style.display = 'block';
+        selectTag.style.background = 'red';
+        selectTag.innerText = 'error pass';
+    } else if (selectInput[1].value.length >= 4) {
+        // selectTag.style.display = 'none';
+        selectTag.style.background = 'rgb(35,149,230)';
+        selectTag.innerText = 'true pass';
+    }
+}
+
+
