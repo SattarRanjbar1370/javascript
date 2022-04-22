27a9aed51b3d3550fdca47831014edb5342b2f4e
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-04-22 13:01:36 +0430

    set event onclick

--- room learn/DOM project/04 select country/root/app/main.js
+++ room learn/DOM project/04 select country/root/app/main.js
@@ -4,4 +4,10 @@
 let selectCode = document.getElementById('code-country');
 let selectForm = document.getElementById('staticEmail2');
 let selectButton = document.querySelectorAll('button');
+let item = document.getElementById('show-item');
 
+selectButton[0].setAttribute('onClick', 'selectModal()');
+
+function selectModal() {
+    item.innerText = selectCode.value + selectForm.value;
+}
