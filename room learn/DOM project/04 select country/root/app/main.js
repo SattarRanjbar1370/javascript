a65439959a8ddd1d187d1a7c99490e65dad1d1a6
SattarRanjbar1370 <iran.kalkoush@gmail.com>
2022-04-22 11:10:08 +0430

    select element dom

---
+++ room learn/DOM project/04 select country/root/app/main.js
@@ -0,0 +1,7 @@
+// 
+
+
+let selectCode = document.getElementById('code-country');
+let selectForm = document.getElementById('staticEmail2');
+let selectButton = document.querySelectorAll('button');
+
