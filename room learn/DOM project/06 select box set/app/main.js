bc62e0fe3fcfad2aafa62702cae0f39555307a7a
sattarranjbar1370 <iran.kalkoush@gmail.com>
2022-04-30 01:24:25 +0430

    add database and selection the elements in index.html and set event

---
+++ room learn/DOM project/06 select box set/app/main.js
@@ -0,0 +1,11 @@
+// data base
+
+const dataCountry = {
+    iran: ['tehran', 'esfahan', 'kermanshah',],
+    usa: ['new yourk', 'vashington'],
+    russia: ['Moscow', 'Saint Petersburg','Samara','Volgagrad']
+};
+
+let selectBoxCountry = document.getElementById('box-country');
+// selectBoxCountry.addEventListener('change', setValue);
+
