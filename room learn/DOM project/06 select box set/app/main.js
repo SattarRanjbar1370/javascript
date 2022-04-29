1bfeff22b683359975c27c9f04badde7e429957b
sattarranjbar1370 <iran.kalkoush@gmail.com>
2022-04-30 01:59:21 +0430

    add function setValue to event onchange for set city

--- room learn/DOM project/06 select box set/app/main.js
+++ room learn/DOM project/06 select box set/app/main.js
@@ -3,9 +3,20 @@
 const dataCountry = {
     iran: ['tehran', 'esfahan', 'kermanshah',],
     usa: ['new yourk', 'vashington'],
-    russia: ['Moscow', 'Saint Petersburg','Samara','Volgagrad']
+    russia: ['Moscow', 'Saint Petersburg', 'Samara', 'Volgagrad']
 };
 
 let selectBoxCountry = document.getElementById('box-country');
-// selectBoxCountry.addEventListener('change', setValue);
+selectBoxCountry.addEventListener('change', setValue);
+let selectCity = document.getElementById('box-city');
+let setElemnt;
 
+function setValue() {
+    selectCity.innerHTML = '<option selected>Select one</option>';
+    dataCountry[selectBoxCountry.value].forEach(element => {
+        setElemnt = document.createElement('option');
+        setElemnt.setAttribute('value', element);
+        setElemnt.innerText = element;
+        selectCity.append(setElemnt);
+    });
+}
